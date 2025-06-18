83a3116970c5e684a9034dffc5645f8d3ae15c33
Mikkareem <mohdirsath1997@gmail.com>
2025-06-18 06:27:51 +0530

    [Feature] Game feature is implemented for initial testing.

--- feature/game/data/build.gradle.kts
+++ feature/game/data/build.gradle.kts
@@ -6,9 +6,12 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(projects.database)
-
+            implementation(projects.core.constants)
             implementation(projects.feature.game.domain)
+
             implementation(libs.ktor.client.core)
+            implementation(libs.koin.core)
+            implementation(libs.kotlinx.serialization.json)
         }
     }
 }
